e68c9335ca32255d9ce5ccb94ce3e4e7fd0d0c44
Dairon Medina Caro <dairon@gooee.com>
2016-09-23 10:47:30 -0400

    Added cowbow endpoints to serve static files

--- src/vmq_dashboard_app.erl
+++ src/vmq_dashboard_app.erl
@@ -10,7 +10,21 @@
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
-    vernemq_demo_plugin_sup:start_link().
+  vernemq_demo_plugin_sup:start_link().
 
 stop(_State) ->
-    ok.
+  ok.
+
+init() ->
+  Dispatch = cowboy_router:compile([
+    {'_', [
+      {"/", cowboy_static, {priv_file, vmq_dashboard, "www/index.html"}},
+      {"/static/[...]", cowboy_static, {priv_dir, vmq_dashboard, "www/static"}}
+    ]}
+  ]),
+  {ok, _} = cowboy:start_http(http, 100, [{port, 9000}], [
+    {env, [{dispatch, Dispatch}]}
+  ]),
+
+
+  ok.
