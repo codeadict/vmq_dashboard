4986a054f64a5ceabc8f68fb4ec339afe5b1c7f4
Dairon Medina Caro <dairon@gooee.com>
2016-11-19 23:41:27 -0500

    Added router using trails to handle statics and REST as well

--- src/vmq_dashboard.erl
+++ src/vmq_dashboard.erl
@@ -1,68 +1,73 @@
+%% Copyright 2016, Dairon Medina <me@dairon.org>
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License"); you may not
+%% use this file except in compliance with the License. You may obtain a copy of
+%% the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+%% WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+%% License for the specific language governing permissions and limitations under
+%% the License.
+
 -module(vmq_dashboard).
 
--behaviour(auth_on_register_hook).
--behaviour(auth_on_subscribe_hook).
--behaviour(auth_on_publish_hook).
+-behaviour(gen_server).
 
--export([auth_on_register/5,
-         auth_on_publish/6,
-         auth_on_subscribe/3]).
+%% OTP API
+-export([start_link/0]).
+-export([stop/0]).
 
-%% This file demonstrates the hooks you typically want to use
-%% if your plugin deals with Authentication or Authorization.
-%%
-%% All it does is:
-%%  - authenticate every user and write the log
-%%  - authorize every PUBLISH and SUBSCRIBE and write it to the log
-%%
-%% You don't need to implement all of these hooks, just the one
-%% needed for your use case.
-%%
-%% IMPORTANT:
-%%  these hook functions run in the session context
-%%
-auth_on_register({_IpAddr, _Port} = Peer, {_MountPoint, _ClientId} = SubscriberId, UserName, Password, CleanSession) ->
-    error_logger:info_msg("auth_on_register: ~p ~p ~p ~p ~p", [Peer, SubscriberId, UserName, Password, CleanSession]),
-    %% do whatever you like with the params, all that matters
-    %% is the return value of this function
-    %%
-    %% 1. return 'ok' -> CONNECT is authenticated
-    %% 2. return 'next' -> leave it to other plugins to decide
-    %% 3. return {ok, [{ModifierKey, NewVal}...]} -> CONNECT is authenticated, but we might want to set some options used throughout the client session:
-    %%      - {mountpoint, NewMountPoint::string}
-    %%      - {clean_session, NewCleanSession::boolean}
-    %% 4. return {error, invalid_credentials} -> CONNACK_CREDENTIALS is sent
-    %% 5. return {error, whatever} -> CONNACK_AUTH is sent
+%% gen_server API
+-export([init/1, handle_call/3]).
+-export([handle_info/2]).
+-export([terminate/2]).
+-export([code_change/3]).
+-export([handle_cast/2]).
 
-    %% we return 'ok'
-    ok.
 
-auth_on_publish(UserName, {_MountPoint, _ClientId} = SubscriberId, QoS, Topic, Payload, IsRetain) ->
-    error_logger:info_msg("auth_on_publish: ~p ~p ~p ~p ~p ~p", [UserName, SubscriberId, QoS, Topic, Payload, IsRetain]),
-    %% do whatever you like with the params, all that matters
-    %% is the return value of this function
-    %%
-    %% 1. return 'ok' -> PUBLISH is authorized
-    %% 2. return 'next' -> leave it to other plugins to decide
-    %% 3. return {ok, NewPayload::binary} -> PUBLISH is authorized, but we changed the payload
-    %% 4. return {ok, [{ModifierKey, NewVal}...]} -> PUBLISH is authorized, but we might have changed different Publish Options:
-    %%     - {topic, NewTopic::string}
-    %%     - {payload, NewPayload::binary}
-    %%     - {qos, NewQoS::0..2}
-    %%     - {retain, NewRetainFlag::boolean}
-    %% 5. return {error, whatever} -> auth chain is stopped, and message is silently dropped (unless it is a Last Will message)
-    %%
-    %% we return 'ok'
-    ok.
+start_link() ->
+    case gen_server:start_link({local, ?MODULE}, ?MODULE, [], []) of
+        {ok, Pid} -> {ok, Pid};
+        {error, {already_started, Pid}} -> {ok, Pid}
+    end.
 
-auth_on_subscribe(UserName, ClientId, [{_Topic, _QoS}|_] = Topics) ->
-    error_logger:info_msg("auth_on_subscribe: ~p ~p ~p", [UserName, ClientId, Topics]),
-    %% do whatever you like with the params, all that matters
-    %% is the return value of this function
-    %%
-    %% 1. return 'ok' -> SUBSCRIBE is authorized
-    %% 2. return 'next' -> leave it to other plugins to decide
-    %% 3. return {error, whatever} -> auth chain is stopped, and no SUBACK is sent
 
-    %% we return 'ok'
-    ok.
+stop() ->
+    gen_server:call(?MODULE, stop).
+
+init(_) ->
+    {ok, Options} = application:get_env(vmq_dashboard, http_server),
+    Port = proplists:get_value(port, Options),
+
+    Handlers =
+        [ cowboy_swagger_handler
+        , vmq_dashboard_http_handler
+        ],
+
+    Routes =
+        [ {"/", cowboy_static, {file, "www/index.html"}}
+        , {"/favicon.ico", cowboy_static, {file, "www/static/favicon.ico"}}
+        , {"/static/[...]", cowboy_static, {dir, "www/static"}}
+          | trails:trails(Handlers)
+        ],
+
+    trails:store(Routes),
+    Dispatch = trails:single_host_compile(Routes),
+    {ok, _} = cowboy:start_http(http, 100, [{port, Port}], [{env, [{dispatch, Dispatch}]}]),
+    {ok, []}.
+
+handle_call(stop, _From, State) ->
+    {stop, normal, stopped, State}.
+
+handle_cast(shutdown, State) ->
+    {stop, normal, State}.
+
+handle_info(_Info, State) -> {noreply, State}.
+
+%% default gen_server callbacks
+terminate(_Reason, _State) ->  ok.
+
+code_change(_OldVsn, State, _Extra) -> {ok, State}.
