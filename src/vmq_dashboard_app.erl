f952bec4c5cf8b2fab704966b04ba13bbe60bfc2
Dairon Medina Caro <dairon@gooee.com>
2016-11-19 23:40:50 -0500

    Added router using trails to handle statics and REST as well

--- src/vmq_dashboard_app.erl
+++ src/vmq_dashboard_app.erl
@@ -1,3 +1,17 @@
+%% Copyright 2016, Dairon Medina <me@dairon.org>
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License"); you may not
+%% use this file except in compliance with the License. You may obtain a copy of
+%% the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+%% WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+%% License for the specific language governing permissions and limitations under
+%% the License.
+
 -module(vmq_dashboard_app).
 
 -behaviour(application).
@@ -5,26 +19,14 @@
 %% Application callbacks
 -export([start/2, stop/1]).
 
-%% ===================================================================
-%% Application callbacks
-%% ===================================================================
+%%====================================================================
+%% API
+%%====================================================================
 
 start(_StartType, _StartArgs) ->
-  vernemq_demo_plugin_sup:start_link().
+    {ok, Sup} = vmq_dashboard_sup:start_link(),
+    {ok, Sup}.
 
+%%--------------------------------------------------------------------
 stop(_State) ->
-  ok.
-
-init() ->
-  Dispatch = cowboy_router:compile([
-    {'_', [
-      {"/", cowboy_static, {priv_file, vmq_dashboard, "www/index.html"}},
-      {"/static/[...]", cowboy_static, {priv_dir, vmq_dashboard, "www/static"}}
-    ]}
-  ]),
-  {ok, _} = cowboy:start_http(http, 100, [{port, 9000}], [
-    {env, [{dispatch, Dispatch}]}
-  ]),
-
-
-  ok.
+    ok.
