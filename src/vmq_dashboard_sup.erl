ed9c262ea3393278f367a193af8e9f2ea1c34bef
Dairon Medina Caro <dairon@gooee.com>
2016-11-19 23:40:13 -0500

    Improved processes supervisor

--- src/vmq_dashboard_sup.erl
+++ src/vmq_dashboard_sup.erl
@@ -1,3 +1,22 @@
+%% Copyright 2016, Dairon Medina <me@dairon.org>
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License"); you may not
+%% use this file except in compliance with the License. You may obtain a copy of
+%% the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+%% WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+%% License for the specific language governing permissions and limitations under
+%% the License.
+
+%%%-------------------------------------------------------------------
+%% @doc vmq_dashboard top level supervisor.
+%% @end
+%%%-------------------------------------------------------------------
+
 -module(vmq_dashboard_sup).
 
 -behaviour(supervisor).
@@ -8,20 +27,21 @@
 %% Supervisor callbacks
 -export([init/1]).
 
-%% Helper macro for declaring children of supervisor
--define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
+-define(SERVER, ?MODULE).
 
-%% ===================================================================
+%%====================================================================
 %% API functions
-%% ===================================================================
+%%====================================================================
 
 start_link() ->
-    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
 
 %% ===================================================================
 %% Supervisor callbacks
 %% ===================================================================
 
-init([]) ->
-    {ok, { {one_for_one, 5, 10}, []} }.
-
+init(_Args) ->
+    SupFlags = #{strategy => one_for_one, intensity => 5, period => 10},
+    ChildSpecs = [#{id => vmq_dashboard,
+                    start => {vmq_dashboard, start_link, []}}],
+    {ok, {SupFlags, ChildSpecs}}.
